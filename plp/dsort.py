e9e3f1e6e0116b8d95a631885fc6e8dc49c17c13
virtosu.bogdan <virtosu.bogdan@gmail.com>
2015-01-10 18:07:00 +0200

    Added dictionary sort

---
+++ plp/dsort.py
@@ -0,0 +1,53 @@
+def read_dicts(filename):
+    with open(filename) as in_file:
+        dicts = []
+        current_dict = {}
+        for line in in_file:
+            if line.isspace():
+                dicts.append(current_dict)
+                current_dict = {}
+            else:
+                try:
+                    a, b = line.split(' ')
+                except ValueError:
+                    raise AttributeError('Value ' + line + ' does not have correct <key><whitespace><value> format')
+                current_dict[a] = int(b)
+        if current_dict is not {}:
+            dicts.append(current_dict)
+        return dicts
+
+
+def d_compare(dict1, dict2):
+    for key1, key2 in zip(sorted(dict1.keys()), sorted(dict2.keys())):
+        val1 = dict1[key1]
+        val2 = dict2[key2]
+        if val1 == val2:
+            continue
+        return val1 > val2
+
+
+def q_sort(l, lo, hi, comparator):
+    if lo >= hi:
+        return l
+    pivot, index = l[hi], lo
+    for i, val in enumerate(l[lo:hi]):
+        if comparator(pivot, val):
+            l[lo + i], l[index], index = l[index], l[lo + i], index + 1
+    l[index], l[hi] = l[hi], l[index]
+    q_sort(l, lo, index - 1, comparator)
+    q_sort(l, index + 1, hi, comparator)
+    return l
+
+
+def d_sort(in_filename, out_filename):
+    dicts = read_dicts(in_filename)
+    ordered_dicts = q_sort(dicts[:], 0, len(dicts) - 1, d_compare)
+    count_appearances = {}
+    with open(out_filename, 'w') as outFile:
+        for item_order in [ordered_dicts.index(x) for x in dicts]:
+            if item_order in count_appearances:
+                outFile.write(str(item_order + count_appearances[item_order]) + '\n')
+                count_appearances[item_order] += 1
+            else:
+                outFile.write(str(item_order) + '\n')
+                count_appearances[item_order] = 1
---
+++ tests/test_dsort.py
@@ -0,0 +1,35 @@
+import unittest
+
+from plp.dsort import d_sort, q_sort
+
+
+class TestDSort(unittest.TestCase):
+    def test_no_file(self):
+        with self.assertRaises(IOError):
+            d_sort('inexistent', 'inexistent')
+
+    def test_simple(self):
+        out_file = '../res/dict.out'
+        open(out_file, 'w').close()
+        d_sort('../res/dict.in', out_file)
+        self.assert_file_order(out_file, [2, 1, 0, 3])
+
+    def test_eq_items(self):
+        out_file = '../res/dict.out'
+        open(out_file, 'w').close()
+        d_sort('../res/dict_eq.in', out_file)
+        self.assert_file_order(out_file, [3, 1, 0, 4, 2, 5])
+
+    def test_sort(self):
+        self.assertEqual(q_sort([1, 5, 0, 2, 2, -2, 2], 0, 5, lambda a, b: a > b), [-2, 0, 1, 2, 2, 5, 2])
+
+    def assert_file_order(self, out_file, expected_order):
+        file_order = []
+        with open(out_file) as out_file:
+            for line in out_file:
+                file_order.append(int(line))
+        self.assertEqual(file_order, expected_order)
+
+
+if __name__ == '__main__':
+    unittest.main()
