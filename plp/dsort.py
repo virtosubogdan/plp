bbe52c16a3e719f19c600c9f98812f1edd4d66e0
Radu Ciorba <radu.ciorba@threepillarglobal.com>
2015-02-16 18:00:11 +0200

    some examples of how we can speed up dsort

--- plp/dsort.py
+++ plp/dsort.py
@@ -1,3 +1,13 @@
+# the speedup suggestions should be taken with a grain of salt
+# they're technically correct, but they don't address the root cause of the slowness
+# that's left as an exercise to the reader
+# hint: the exercise talks about the input and output being dictionaries,
+# but doesn't require you use dictionaries to implement
+
+
+from itertools import izip
+
+
 def read_dicts(filename):
     with open(filename) as in_file:
         dicts = []
@@ -17,10 +27,57 @@ def read_dicts(filename):
         return dicts
 
 
+# %timeit dsort.d_compare(d1, d2)
+# 10000 loops, best of 3: 156 us per loop
 def d_compare(dict1, dict2):
     for key1, key2 in zip(sorted(dict1.keys()), sorted(dict2.keys())):
         val1 = dict1[key1]
         val2 = dict2[key2]
+        if val1 == val2:
+            continue
+        return val1 > val2
+
+
+# %timeit dsort.d_compare_izip(d1, d2)
+# 10000 loops, best of 3: 139 us per loop
+# if we use izip instead of zip, we don't need to build the whole list
+# in memory up front, this has modest improvements
+def d_compare_izip(dict1, dict2):
+    for key1, key2 in izip(sorted(dict1.keys()), sorted(dict2.keys())):
+        val1 = dict1[key1]
+        val2 = dict2[key2]
+        if val1 == val2:
+            continue
+        return val1 > val2
+
+
+# %timeit dsort.d_compare_sort(d1, d2)
+# 10000 loops, best of 3: 129 us per loop
+# since keys returns a list that's safe to mutate,
+# we can sort in place, instead of calling sorted to get a new list
+def d_compare_sort(dict1, dict2):
+    keys1, keys2 = dict1.keys(), dict2.keys()
+    keys1.sort()
+    keys2.sort()
+    for key1, key2 in izip(keys1, keys2):
+        val1 = dict1[key1]
+        val2 = dict2[key2]
+        if val1 == val2:
+            continue
+        return val1 > val2
+
+
+# %timeit dsort.d_compare_items(d1, d2)
+# 1000 loops, best of 3: 284 us per loop
+# what if we eliminate the dictionary lookup?
+# let's call items to get a list of tuples, and sort that
+# nope, this is a major slowdown; probably from the overhead of allocating
+# a lot more new objects
+def d_compare_items(dict1, dict2):
+    items1, items2 = dict1.items(), dict2.items()
+    items1.sort()
+    items2.sort()
+    for (key1, val1), (key2, val2) in izip(items1, items2):
         if val1 == val2:
             continue
         return val1 > val2
